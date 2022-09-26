687ed6b1bd186216641ff19534c934a78e55ce86
Paulo Carabuena <pawekzcarabuena@gmail.com>
2022-09-26 19:40:44 +0800

    Initial commit, throws error code V008 on PVS studio ut the files are compilable and running

---
+++ multiArray.c
@@ -0,0 +1,8 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+#include<stdio.h>
+#include "multiArray.h"
+
+void print(){
+    printf("Hello World");
+}
